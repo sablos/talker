513f759454e81f3a7cece1a6e731fb7564392e7d
Paul Pedro <47475171+sablos@users.noreply.github.com>
2021-11-18 15:42:04 +0000

    allows repetition of running

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-name = input("Tell me your name: ")
-times = int(input("How many times should I say hello? "))
-for i in range(times):
-  print(f"Hello {name}!")
+again = True
+while again:
+  name = input("Who should I say hello to?: ")
+  times = int(input("How many times should I say hello? "))
+  for i in range(times):
+    print(f"Hello {name}!")
+  print()
+  choice = input("Say hello to someone else? (y/n) ")
+  if choice != "y":
+    again = False
