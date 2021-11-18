6dc5203eb5c8a4511ce240252afb0b3eff53b854
Paul Pedro <47475171+sablos@users.noreply.github.com>
2021-11-18 20:56:51 +0000

    Added functions for talk and listen

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+def talk(statement):
+  print("< ~ " + statement)
+
+def listen():
+  return input(">>> ")
+
 # program intro
 print("This program says hello to the person of your choice")
 print()
 again = True
 while again:
-  print(" ~ Who should I say hello to?: ")
-  name = input(">>> ")
-  print(" ~ How many times should I say hello? ")
-  times = input(">>> ")
+  talk("Who should I say hello to?: ")
+  name = listen()
+  talk("How many times should I say hello? ")
+  times = listen()
   # check if times is a valid number with isdigit()
   if times.isdigit():
     times = int(times)
     for i in range(times):
-      print(f" ~ Hello {name}!")
+      talk(f"Hello {name}!")
   else:
-    print(" ~ You didn't enter a number so I'll say hello once")
-    print(f" ~ Hello {name}!")
+    talk("You didn't enter a number so I'll say hello once")
+    talk(f"Hello {name}!")
   print()
-  print(" ~ Say hello to someone else? (y/n) ")
-  choice = input(">>> ")
+  talk("Say hello to someone else? (y/n) ")
+  choice = listen()
   if choice != "y":
     again = False
