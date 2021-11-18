74081aa539989b5b9ec1560c22ca3c1fa4f06236
Paul Pedro <47475171+sablos@users.noreply.github.com>
2021-11-18 20:52:00 +0000

    Added speech markers for user and talkie

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+# program intro
+print("This program says hello to the person of your choice")
+print()
 again = True
 while again:
-  name = input("Who should I say hello to?: ")
-  times = input("How many times should I say hello? ")
-  # check if times is a valid number
+  print(" ~ Who should I say hello to?: ")
+  name = input(">>> ")
+  print(" ~ How many times should I say hello? ")
+  times = input(">>> ")
+  # check if times is a valid number with isdigit()
   if times.isdigit():
     times = int(times)
     for i in range(times):
-      print(f"Hello {name}!")
+      print(f" ~ Hello {name}!")
   else:
-    print("You didn't enter a number so I'll say hello once")
-    print(f"Hello {name}!")
+    print(" ~ You didn't enter a number so I'll say hello once")
+    print(f" ~ Hello {name}!")
   print()
-  choice = input("Say hello to someone else? (y/n) ")
+  print(" ~ Say hello to someone else? (y/n) ")
+  choice = input(">>> ")
   if choice != "y":
     again = False
