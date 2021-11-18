a24430e0cf86a1ff80187da7410bcc5a4eada2d0
Paul Pedro <47475171+sablos@users.noreply.github.com>
2021-11-18 16:15:40 +0000

    prevents integer error

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 again = True
 while again:
   name = input("Who should I say hello to?: ")
-  times = int(input("How many times should I say hello? "))
-  for i in range(times):
+  times = input("How many times should I say hello? ")
+  # check if times is a valid number
+  if times.isdigit():
+    times = int(times)
+    for i in range(times):
+      print(f"Hello {name}!")
+  else:
+    print("You didn't enter a number so I'll say hello once")
     print(f"Hello {name}!")
   print()
   choice = input("Say hello to someone else? (y/n) ")
